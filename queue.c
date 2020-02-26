3340e7fad0556f47457060c6dcac90e6b9be84c0
Hsuhaoxiang <r07922163@ntu.edu.tw>
2020-02-26 14:20:15 +0800

    Finish q_insert_head

--- queue.c
+++ queue.c
@@ -36,11 +36,18 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return false;
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    newh = (list_ele_t *) malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+    newh->value = (char *) malloc(sizeof(char));
+    if (!(newh->value)) {
+        free(newh);
+        return false;
+    }
+    strncpy(newh->value, s, strlen(s) + 1);
     newh->next = q->head;
     q->head = newh;
     return true;
